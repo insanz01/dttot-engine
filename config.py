f5dffc6a4d09cbb642372543814fe151629f76c6
Insan Kamil <insankamil002@gmail.com>
2024-04-09 10:09:31 +0700

    add db port on config

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ port = os.environ.get('APP_PORT', 5000)
 db_name = os.environ.get('DB_NAME', 'dttot-engine')
 db_user = os.environ.get('DB_USER', 'root')
 db_password = os.environ.get('DB_PASSWORD', 'password')
+db_port = os.environ.get('DB_PORT', '5432')
 
 
 class Config(object):
@@ -13,7 +14,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'XXXXX'  # todo: should be changed for production
-    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@database:5432/{}".format(db_user, db_password, db_name)
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@database:{}/{}".format(db_user, db_password, db_port, db_name)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECURITY_URL_PREFIX = ""
