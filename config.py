aa604ae7afddb91e3fc3f1767a7cc6adaeeba774
Insan Kamil <insankamil002@gmail.com>
2024-04-09 10:08:51 +0700

    add db config in config.py

--- config.py
+++ config.py
@@ -3,12 +3,18 @@ import os
 host = os.environ.get('APP_HOST', 'localhost')
 port = os.environ.get('APP_PORT', 5000)
 
+db_name = os.environ.get('DB_NAME', 'dttot-engine')
+db_user = os.environ.get('DB_USER', 'root')
+db_password = os.environ.get('DB_PASSWORD', 'password')
+
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'XXXXX'  # todo: should be changed for production
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@database:5432/{}".format(db_user, db_password, db_name)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECURITY_URL_PREFIX = ""
     SECURITY_PASSWORD_HASH = "pbkdf2_sha512"
